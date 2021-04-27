25513605e6b950a124d7e2f38f5c058fdadf148a
Gregor-von-Vitek <gregorvonvitek@gmail.com>
2021-04-27 19:27:15 +0200

    sticky header completion

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -14,7 +14,6 @@ class RevealOnScroll {
     events() {
         window.addEventListener("scroll", this.scrollThrottle);
         window.addEventListener("resize", debounce(() => {
-            console.log("resize test");
             this.browserHeight = window.innerHeight;
         }, 333));
     }
