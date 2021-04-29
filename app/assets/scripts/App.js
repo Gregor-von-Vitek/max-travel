9b503aa124b68fc03eaed9a73c23729a7d743846
Gregor-von-Vitek <gregorvonvitek@gmail.com>
2021-04-29 11:39:20 +0200

    "minor change"

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
-
-new StickyHeader();
 
 let modal; 
 
