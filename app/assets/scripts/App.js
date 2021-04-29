474a40664ed11ce79c98a4742f9f1dffe83c31d5
Gregor-von-Vitek <gregorvonvitek@gmail.com>
2021-04-29 11:36:48 +0200

    modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,6 +10,22 @@ new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
 
 new StickyHeader();
 
+let modal; 
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */"./modules/Modal").then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem"))
+    } else {
+      modal.openTheModal();
+    }
+  })
+})
+
 if (module.hot) {
   module.hot.accept()
 }
