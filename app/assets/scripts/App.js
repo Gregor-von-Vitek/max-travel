bbaf21dc0474ae7187cf41f99bf986edfa6d5cd5
Gregor-von-Vitek <gregorvonvitek@gmail.com>
2021-05-10 14:43:22 +0200

    preparation for build

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,27 +1,25 @@
-import '../styles/styles.css';
-import "lazysizes";
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
+import '../styles/styles.css'
+import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
-new StickyHeader();
-
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
-
-let modal; 
+let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
-    e.preventDefault();
+    e.preventDefault()
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */"./modules/Modal").then(x => {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("There was a problem"))
+      }).catch(() => console.log("There was a problem."))
     } else {
-      modal.openTheModal();
+      modal.openTheModal()
     }
   })
 })
@@ -29,4 +27,3 @@ document.querySelectorAll(".open-modal").forEach(el => {
 if (module.hot) {
   module.hot.accept()
 }
-
